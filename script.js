041b233576c6aafc160dbe1aa32df1e8f164f8ec
Dananjana Wijesooriya <dananjanawijesooriya@gmail.com>
2026-06-01 14:47:09 +0530

    Update files

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function showMessage(){
     document.getElementById("message").innerHTML="CI/CD Pipeline Working!";
 }
+function showName(){
+    document.getElementById("message").innerHTML="CDW";
+}
